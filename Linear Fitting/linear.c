fe555bbda6f23c2b6f6322cc5c9d85798864ee92
Dipesh Kumar Mandal <dipeshmandal160@gmail.com>
2022-11-13 12:59:50 +0545

    done

---
+++ Linear Fitting/linear.c
@@ -0,0 +1,21 @@
+// linear fitting y=a+b*c
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    float x[10];
+    printf("Enter no. of data point\n");
+    scanf("%d",&n);
+    printf("Enter value of x: \n");
+    for (int i = 0; i <= n; i++)
+    {
+        scanf("%f",&x[i]);
+    }
+    printf("Enter value of y:\n");
+    for(i=1; i<=n; i++){
+        scanf("%f",&y[i]);
+    }
+    ; 
+    
+}
